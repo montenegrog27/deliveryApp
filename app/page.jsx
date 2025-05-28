f35778a68db72791c699859a9443a513cf4d783a
montenegrog27 <german2717@gmail.com>
2025-05-28 00:51:47 -0300

    a

--- app/page.jsx
+++ app/page.jsx
@@ -6,11 +6,11 @@ export default function Home() {
       <h1 className="text-5xl md:text-6xl font-extrabold tracking-tight mb-3  text-[#fff9f5] ">
         Mordisco burgers
       </h1>
-          <img
-          src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1744755147/Versi%C3%B3n_principal_xer7zs.svg"
-          alt="MORDISCO"
-          className="h-8"
-        />
+      <img
+        src="https://res.cloudinary.com/dsbrnqc5z/image/upload/v1744755147/Versi%C3%B3n_principal_xer7zs.svg"
+        alt="MORDISCO"
+        className="h-8"
+      />
       <p className="text-lg text-neutral-600 font-medium mb-6">
         Corrientes Capital, Argentina 🍔
       </p>
@@ -19,7 +19,8 @@ export default function Home() {
       <div className="bg-white p-6 md:p-8 rounded-2xl shadow-md max-w-xl w-full mb-8 border border-neutral-200">
         <h2 className="text-2xl font-bold mb-2">Sitio en construcción</h2>
         <p className="text-base text-neutral-700 mb-4">
-          Muy pronto vas a poder hacer tu pedido online y disfrutar nuestras burgers desde casa.
+          Muy pronto vas a poder hacer tu pedido online y disfrutar nuestras
+          burgers desde casa.
         </p>
         <button
           disabled
@@ -32,7 +33,9 @@ export default function Home() {
       {/* Información obligatoria para Meta */}
       <div className="text-left text-sm md:text-base space-y-4 text-neutral-700 max-w-md w-full">
         <div>
-          <h3 className="font-semibold text-neutral-800 mb-1">📍 Dirección comercial</h3>
+          <h3 className="font-semibold text-neutral-800 mb-1">
+            📍 Dirección comercial
+          </h3>
           <p>Calle Santa Fe 1583, Corrientes Capital, Argentina</p>
         </div>
 
@@ -43,7 +46,9 @@ export default function Home() {
         </div>
 
         <div>
-          <h3 className="font-semibold text-neutral-800 mb-1">📲 Redes Sociales</h3>
+          <h3 className="font-semibold text-neutral-800 mb-1">
+            📲 Redes Sociales
+          </h3>
           <a
             href="https://instagram.com/mordisco.arg"
             target="_blank"
@@ -52,13 +57,16 @@ export default function Home() {
           >
             instagram.com/mordisco.arg
           </a>
-                    <a
+        </div>
+
+        <div>
+          <a
             href="https://www.facebook.com/profile.php?id=61575119946351"
             target="_blank"
             rel="noopener noreferrer"
             className="text-blue-600 underline"
           >
-            facboo.com/mordisco
+            facebook.com/mordisco
           </a>
         </div>
       </div>
